f2174f0bdd0afc01d6aac734ad6de3e02aebe165
Christian Molino <cmolino@finfrock.com>
2024-08-07 20:05:35 -0400

    Added 2023 debug and release configurations

--- DualDeckEditorAddin/DualDeckEditorCommand.cs
+++ DualDeckEditorAddin/DualDeckEditorCommand.cs
@@ -13,6 +13,16 @@ namespace DualDeckEditorAddin
     [Transaction(TransactionMode.Manual)]
     public class DualDeckEditorCommand : IExternalCommand
     {
+        private string getVersion()
+        {
+#if REVIT2021
+    return "Revit 2021";
+#elif REVIT2022
+    return "Revit 2022";
+#else
+    return "Revit 2023";
+#endif
+        }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication uiApp = commandData.Application;
